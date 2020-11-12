9347c5728f4792ca9ac28dd56d0aff518c232712
MarcCornellas <65563018+MarcCornellas@users.noreply.github.com>
2020-11-12 18:20:15 +0100

    Initial Commit

---
+++ Editor/ReadAndCheck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Text;
+
+public class ReadAndCheck : EditorWindow
+{
+    [MenuItem("PSIOUS/CheckFiles")]
+
+    //public GameObject recallTextObject;
+    public static void ShowWindow()
+    {
+        GetWindow<ReadAndCheck>("CheckFiles");
+    }
+ 
+
+    void OnGUI()
+    {
+        GUILayout.Label("PSIOUS", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("CheckFiles"))
+        {
+            ReadFile();
+        }
+    }
+
+    void ReadFile()
+    {
+        string readFromFilePath = Application.dataPath + "/txt_Doc/DirectoriesPsious.txt"; 
+        string[] readText = File.ReadAllLines(readFromFilePath);
+
+        foreach (string line in readText)
+        {
+            if(line != "Assets")
+            {
+                if (!Directory.Exists(Application.dataPath + "/" + line))
+                    Debug.Log("No existe este fichero: " + line);
+            }
+        }
+    }
+}
